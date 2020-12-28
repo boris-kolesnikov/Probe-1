71241316bc90249e94a49391fb23424102df9ac4
boris-kolesnikov <boris.kolesnikov.1997@gmail.com>
2020-12-28 14:44:56 +0300

    Update fumctiya_v_cikle.py

--- fumctiya_v_cikle.py
+++ fumctiya_v_cikle.py
@@ -1,12 +1,12 @@
-def print_friends_count(friends_count):
-    remainder = friends_count % 10
-    if friends_count == 0:
+def print_money_count(money_count):
+    remainder = money_count % 10
+    if money_count == 0:
         print('У тебя нет денег')
-    elif remainder == 0 or remainder >= 5 or (10 <= friends_count <= 19):
-        print('У тебя', friends_count, 'рублей')
+    elif remainder == 0 or remainder >= 5 or (10 <= money_count <= 19):
+        print('У тебя', money_count, 'рублей')
     elif remainder == 1:
-        print('У тебя', friends_count, 'рубль')
+        print('У тебя', money_count, 'рубль')
     else:
-        print('У тебя', friends_count, 'рубля')
+        print('У тебя', money_count, 'рубля')
 for i in range(1,11):
-    print(print_friends_count(i))
+    print(print_money_count(i))
